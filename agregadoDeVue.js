f3bff4e3e81302b8d0d2e866767bfc05b51d9110
samuel <129302031+samuelfrencia@users.noreply.github.com>
2023-05-17 20:33:06 -0300

    Create agregadoDeVue.js

---
+++ agregadoDeVue.js
@@ -0,0 +1,13 @@
+const carrouselVue = Vue.createApp({
+    data() {
+        return {
+            imgCarrousel1: 'img/DC_20221104105427_pTu1uEmz.jpg',
+
+            imgCarrousel2: 'img/DC_20230110092310_VJe69EQR.jpg',
+
+            imgCarrousel3: 'img/DC_20230315140823_a1xsazfM.jpg'
+
+            
+        }
+    }
+})
